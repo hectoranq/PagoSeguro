d976a256b6f134eb419f730d6de628e13f3c3895
Ivan H. Aquino Apaza <39768049+hectoranq@users.noreply.github.com>
2025-09-29 01:31:34 +0000

    create confirm pago

--- src/app/components/purchase-data-form/purchase-data-form.component.ts
+++ src/app/components/purchase-data-form/purchase-data-form.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LucideAngularModule } from 'lucide-angular';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-purchase-data-form',
   standalone: true,
@@ -9,8 +11,12 @@ import { LucideAngularModule } from 'lucide-angular';
   styleUrls: ['./purchase-data-form.component.scss']
 })
 export class PurchaseDataFormComponent {
+  constructor(private router: Router) {}
 
   siguiente() {
-    alert('Formulario enviado');
+    this.router.navigate(['/confirm-payment'], {
+      replaceUrl: true,
+      state: { registerDevice: true },
+    });
   }
 }
